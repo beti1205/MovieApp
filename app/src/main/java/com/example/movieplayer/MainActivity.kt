37cab1f999d050b5248f298a94f5d2e491c7848a
beti1205 <beta.bujalska@gmail.com>
2022-09-12 13:43:43 +0200

    fix: Clear search state - MainActivity

--- app/src/main/java/com/example/movieplayer/MainActivity.kt
+++ app/src/main/java/com/example/movieplayer/MainActivity.kt
@@ -5,8 +5,10 @@ import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.isVisible
 import androidx.databinding.DataBindingUtil
+import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupWithNavController
 import com.example.movieplayer.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
@@ -18,6 +20,9 @@ class MainActivity : AppCompatActivity() {
     val searchEditText: EditText
         get() = binding.searchEditFrame
 
+    private lateinit var appBarConfiguration: AppBarConfiguration
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -26,19 +31,12 @@ class MainActivity : AppCompatActivity() {
             R.layout.activity_main
         )
 
-        val navHostFragment =
-            supportFragmentManager.findFragmentById(R.id.myNavHostFragment) as NavHostFragment
-        val navController = navHostFragment.navController
-        binding.bottomNavigation.setupWithNavController(navController)
+        val navHostFragment = supportFragmentManager.findFragmentById(
+            R.id.myNavHostFragment
+        ) as NavHostFragment
+        navController = navHostFragment.navController
 
-        navController.addOnDestinationChangedListener { _, destination, _ ->
-            binding.bottomNavigation.isVisible =
-                destination.id != R.id.movieDetailsFragment && destination.id != R.id.TVSeriesDetailsFragment
-            binding.searchEditFrame.isVisible =
-                destination.id == R.id.searchMoviesFragment || destination.id == R.id.searchTvSeriesFragment
-            binding.searchEditFrame.setText("")
-        }
-        val appBarConfiguration = AppBarConfiguration(
+        appBarConfiguration = AppBarConfiguration(
             setOf(
                 R.id.movieFragment,
                 R.id.TVSeriesFragment
@@ -47,5 +45,24 @@ class MainActivity : AppCompatActivity() {
 
         setSupportActionBar(binding.topAppBar)
         binding.topAppBar.setupWithNavController(navController, appBarConfiguration)
+        binding.topAppBar.setNavigationOnClickListener {
+            onBackPressedDispatcher.onBackPressed()
+        }
+        binding.bottomNavigation.setupWithNavController(navController)
+
+        navController.addOnDestinationChangedListener { _, destination, _ ->
+            binding.bottomNavigation.isVisible = destination.id in listOf(
+                R.id.movieFragment,
+                R.id.TVSeriesFragment
+            )
+            binding.searchEditFrame.isVisible = destination.id in listOf(
+                R.id.searchMoviesFragment,
+                R.id.searchTvSeriesFragment
+            )
+        }
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
     }
 }

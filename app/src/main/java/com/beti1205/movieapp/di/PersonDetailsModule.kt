8ec36996ad2964c9af0ac2e706aaeb33af4c10b7
beti1205 <beta.bujalska@gmail.com>
2022-11-26 21:40:51 +0100

    feature: Fetch person details - data and domain layer

---
+++ app/src/main/java/com/beti1205/movieapp/di/PersonDetailsModule.kt
@@ -0,0 +1,29 @@
+package com.beti1205.movieapp.di
+
+import com.beti1205.movieapp.feature.fetchpersondetails.data.PersonDetailsService
+import com.beti1205.movieapp.feature.fetchpersondetails.domain.FetchPersonDetailsUseCase
+import com.beti1205.movieapp.feature.fetchpersondetails.domain.FetchPersonDetailsUseCaseImpl
+import dagger.Binds
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.components.ViewModelComponent
+import retrofit2.Retrofit
+import retrofit2.create
+
+@Module
+@InstallIn(ViewModelComponent::class)
+interface PersonDetailsModule {
+
+    companion object {
+        @Provides
+        fun providePersonDetailsService(
+            retrofit: Retrofit
+        ): PersonDetailsService = retrofit.create()
+    }
+
+    @Binds
+    fun bindPersonDetailsUseCase(
+        fetchPersonDetailsUseCaseImpl: FetchPersonDetailsUseCaseImpl
+    ): FetchPersonDetailsUseCase
+}

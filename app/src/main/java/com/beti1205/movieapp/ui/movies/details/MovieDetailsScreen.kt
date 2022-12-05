0c72084f33fc95368a1d11cf4d2c4e3e90068dcc
beti1205 <beta.bujalska@gmail.com>
2022-12-05 21:02:20 +0100

    refactor: Movie details screen state - split composables

--- app/src/main/java/com/beti1205/movieapp/ui/movies/details/MovieDetailsScreen.kt
+++ app/src/main/java/com/beti1205/movieapp/ui/movies/details/MovieDetailsScreen.kt
@@ -1,24 +1,15 @@
 package com.beti1205.movieapp.ui.movies.details
 
 import android.content.res.Configuration
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
-import com.beti1205.movieapp.R
-import com.beti1205.movieapp.ui.common.widget.Details
-import com.beti1205.movieapp.ui.common.widget.StandardDivider
-import com.beti1205.movieapp.ui.movies.details.widget.CastList
-import com.beti1205.movieapp.ui.movies.details.widget.CrewList
 import com.beti1205.movieapp.ui.movies.details.widget.EmptyStateMessage
-import com.beti1205.movieapp.ui.movies.details.widget.SectionTitle
+import com.beti1205.movieapp.ui.movies.details.widget.MovieDetails
 import com.beti1205.movieapp.ui.theme.MovieAppTheme
 
 @Composable
@@ -44,31 +35,12 @@ fun MovieDetailsScreen(
             if (state.hasError) {
                 EmptyStateMessage()
             } else {
-                Column(modifier = Modifier.verticalScroll(rememberScrollState())) {
-                    state.movieDetails?.let { movieDetails ->
-                        Details(
-                            posterPath = movieDetails.posterPath,
-                            title = movieDetails.title,
-                            votes = movieDetails.votes,
-                            releaseDate = movieDetails.releaseDate,
-                            overview = movieDetails.overview,
-                            genres = movieDetails.genres
-                        )
-                    }
-
-                    StandardDivider()
-                    SectionTitle(text = stringResource(id = R.string.cast))
-                    CastList(
-                        cast = state.credits?.cast,
-                        onPersonClicked = onPersonClicked
-                    )
-                    StandardDivider()
-                    SectionTitle(text = stringResource(id = R.string.crew))
-                    CrewList(
-                        crew = state.credits?.crew,
-                        onPersonClicked = onPersonClicked
-                    )
-                }
+                MovieDetails(
+                    movieDetails = state.movieDetails,
+                    cast = state.credits?.cast,
+                    crew = state.credits?.crew,
+                    onPersonClicked = onPersonClicked
+                )
             }
         }
     }
@@ -82,12 +54,16 @@ fun MovieDetailsScreen(
 )
 @Composable
 fun MovieDetailsScreenPreview() {
-    MovieDetailsScreen(
-        state = MovieDetailsScreenState(
-            movieDetails = MovieDetailsPreviewDataProvider.movie,
-            credits = MovieDetailsPreviewDataProvider.credits,
-            hasError = false
-        ),
-        onPersonClicked = {}
-    )
+    MovieAppTheme {
+        Surface {
+            MovieDetailsScreen(
+                state = MovieDetailsScreenState(
+                    movieDetails = MovieDetailsPreviewDataProvider.movie,
+                    credits = MovieDetailsPreviewDataProvider.credits,
+                    hasError = false
+                ),
+                onPersonClicked = {}
+            )
+        }
+    }
 }
---
+++ app/src/main/java/com/beti1205/movieapp/ui/movies/details/widget/MovieDetails.kt
@@ -0,0 +1,56 @@
+package com.beti1205.movieapp.ui.movies.details.widget
+
+import android.content.res.Configuration
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.beti1205.movieapp.feature.fetchcredits.data.Cast
+import com.beti1205.movieapp.feature.fetchcredits.data.Crew
+import com.beti1205.movieapp.feature.fetchmoviedetails.data.MovieDetails
+import com.beti1205.movieapp.ui.movies.details.MovieDetailsPreviewDataProvider
+import com.beti1205.movieapp.ui.theme.MovieAppTheme
+
+@Composable
+fun MovieDetails(
+    movieDetails: MovieDetails?,
+    cast: List<Cast>?,
+    crew: List<Crew>?,
+    onPersonClicked: (Int) -> Unit,
+    modifier: Modifier = Modifier
+) {
+    Column(modifier = modifier.verticalScroll(rememberScrollState())) {
+        SectionMovie(movieDetails = movieDetails)
+        SectionCast(
+            cast = cast,
+            onPersonClicked = onPersonClicked
+        )
+        SectionCrew(
+            crew = crew,
+            onPersonClicked = onPersonClicked
+        )
+    }
+}
+
+@Preview
+@Preview(
+    uiMode = Configuration.UI_MODE_NIGHT_YES,
+    showBackground = true,
+    heightDp = 2000
+)
+@Composable
+fun MovieDetailsScreenPreview() {
+    MovieAppTheme {
+        Surface {
+            MovieDetails(
+                movieDetails = MovieDetailsPreviewDataProvider.movie,
+                cast = MovieDetailsPreviewDataProvider.credits.cast,
+                crew = MovieDetailsPreviewDataProvider.credits.crew,
+                onPersonClicked = {}
+            )
+        }
+    }
+}

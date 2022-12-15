6ba8174f8881f4053d7e50ba62e87fa794a33127
beti1205 <beta.bujalska@gmail.com>
2022-12-15 21:35:47 +0100

    feature: TV series details credits - data and domain layer

---
+++ app/src/main/java/com/beti1205/movieapp/feature/fetchcredits/domain/TransformCredits.kt
@@ -0,0 +1,33 @@
+package com.beti1205.movieapp.feature.fetchcredits.domain
+
+import com.beti1205.movieapp.common.Result
+import com.beti1205.movieapp.feature.fetchcredits.data.Cast
+import com.beti1205.movieapp.feature.fetchcredits.data.Credits
+import com.beti1205.movieapp.feature.fetchcredits.data.Crew
+
+fun Result.Success<Credits>.getTransformedCastList() = this.data.cast
+    .groupBy { person -> person.id }
+    .map { entry ->
+        val firstRole = entry.value.first()
+        Cast(
+            id = entry.key,
+            name = firstRole.name,
+            popularity = firstRole.popularity,
+            path = firstRole.path,
+            character = entry.value.joinToString { it.character }
+        )
+    }
+
+fun Result.Success<Credits>.getTransformedCrewList() = this.data.crew
+    .groupBy { person -> person.id }
+    .map { entry ->
+        val firstRole = entry.value.first()
+        Crew(
+            id = entry.key,
+            department = entry.value.joinToString { it.department },
+            name = firstRole.name,
+            popularity = firstRole.popularity,
+            path = firstRole.path,
+            job = entry.value.joinToString { it.job }
+        )
+    }

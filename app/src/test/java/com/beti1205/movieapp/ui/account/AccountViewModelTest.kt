6e3962e17de7a6fe721a803e3e6ecd6698706d37
beti1205 <beta.bujalska@gmail.com>
2023-01-18 19:54:15 +0100

    feature: Account details - add tests

--- app/src/test/java/com/beti1205/movieapp/ui/account/AccountViewModelTest.kt
+++ app/src/test/java/com/beti1205/movieapp/ui/account/AccountViewModelTest.kt
@@ -8,6 +8,9 @@ import com.beti1205.movieapp.common.Result
 import com.beti1205.movieapp.feature.createsession.data.SessionResponse
 import com.beti1205.movieapp.feature.createsession.domain.CreateSessionUseCase
 import com.beti1205.movieapp.feature.deletesession.domain.DeleteSessionUseCase
+import com.beti1205.movieapp.feature.fetchaccountdetails.data.AccountDetails
+import com.beti1205.movieapp.feature.fetchaccountdetails.data.Avatar
+import com.beti1205.movieapp.feature.fetchaccountdetails.data.Tmdb
 import com.beti1205.movieapp.feature.fetchaccountdetails.domain.FetchAccountDetailsUseCase
 import com.beti1205.movieapp.feature.fetchrequesttoken.data.RequestTokenResponse
 import com.beti1205.movieapp.feature.fetchrequesttoken.domain.FetchRequestTokenUseCase
@@ -219,6 +222,58 @@ class AccountViewModelTest {
     }
 
     @Test
+    fun getAccountDetails_success() = runTest {
+        coEvery { fetchAccountDetailsUseCase() } returns accountDetails
+        every { authManager.isLoggedIn } returns flowOf(true)
+        val state = SavedStateHandle()
+
+        viewModel = AccountViewModel(
+            state,
+            fetchRequestTokenUseCase,
+            createSessionUseCase,
+            deleteSessionUseCase,
+            fetchAccountDetailsUseCase,
+            authManager
+        )
+
+        viewModel.getAccountDetails()
+
+        val collectJob = launch(UnconfinedTestDispatcher()) {
+            viewModel.account.collect()
+        }
+
+        assertEquals(accountDetails.data, viewModel.account.value)
+
+        collectJob.cancel()
+    }
+
+    @Test
+    fun getAccountDetails_failed() = runTest {
+        coEvery { fetchAccountDetailsUseCase() } returns Result.Error(Exception())
+        every { authManager.isLoggedIn } returns flowOf(true)
+        val state = SavedStateHandle()
+
+        viewModel = AccountViewModel(
+            state,
+            fetchRequestTokenUseCase,
+            createSessionUseCase,
+            deleteSessionUseCase,
+            fetchAccountDetailsUseCase,
+            authManager
+        )
+
+        viewModel.getAccountDetails()
+
+        val collectJob = launch(UnconfinedTestDispatcher()) {
+            viewModel.account.collect()
+        }
+
+        assertNull(viewModel.account.value)
+
+        collectJob.cancel()
+    }
+
+    @Test
     fun onErrorHandled_verifyThatFalseWasSet() = runTest {
         coEvery { fetchRequestTokenUseCase() } returns Result.Error(Exception())
         every { authManager.isLoggedIn } returns flowOf(false)
@@ -248,6 +303,25 @@ class AccountViewModelTest {
         collectJob.cancel()
     }
 
+    @Test
+    fun onDeniedHandled_verifyThatFalseWasSet() = runTest {
+        val state = SavedStateHandle(mapOf("denied" to true, "request_token" to null))
+        every { authManager.isLoggedIn } returns flowOf(false)
+
+        viewModel = AccountViewModel(
+            state,
+            fetchRequestTokenUseCase,
+            createSessionUseCase,
+            deleteSessionUseCase,
+            fetchAccountDetailsUseCase,
+            authManager
+        )
+
+        viewModel.onDeniedHandled()
+
+        assertFalse(state["denied"]!!)
+    }
+
     companion object {
         val getTokenSuccess = Result.Success(
             RequestTokenResponse(
@@ -260,6 +334,14 @@ class AccountViewModelTest {
             SessionResponse(
                 success = true,
                 sessionId = "sessionId"
+            )
+        )
+        val accountDetails = Result.Success(
+            AccountDetails(
+                id = 1,
+                name = "Beata",
+                username = "beti1205",
+                avatar = Avatar(tmdb = Tmdb(avatarPath = null))
             )
         )
     }

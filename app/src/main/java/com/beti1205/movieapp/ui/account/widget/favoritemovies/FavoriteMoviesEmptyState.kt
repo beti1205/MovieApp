abc4e647eb91f97f78db1344acb6e579dcca548d
beti1205 <beta.bujalska@gmail.com>
2023-01-29 22:54:57 +0100

    feature: Favorite movies - empty state

---
+++ app/src/main/java/com/beti1205/movieapp/ui/account/widget/favoritemovies/FavoriteMoviesEmptyState.kt
@@ -0,0 +1,59 @@
+package com.beti1205.movieapp.ui.account.widget.favoritemovies
+
+import android.content.res.Configuration
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material.Icon
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.WebAsset
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.beti1205.movieapp.R
+import com.beti1205.movieapp.ui.theme.MovieAppTheme
+import com.beti1205.movieapp.ui.theme.SonicSilver
+
+@Composable
+fun FavoriteMoviesEmptyState() {
+    Column(
+        modifier = Modifier
+            .padding(all = 8.dp)
+            .fillMaxWidth(),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Icon(
+            imageVector = Icons.Filled.WebAsset,
+            contentDescription = "",
+            modifier = Modifier
+                .height(100.dp)
+                .width(100.dp),
+            tint = SonicSilver
+        )
+        Text(
+            text = stringResource(R.string.favorite_movies_empty_state_message),
+            color = SonicSilver
+        )
+    }
+}
+
+@Preview
+@Preview(
+    uiMode = Configuration.UI_MODE_NIGHT_YES,
+    showBackground = true
+)
+@Composable
+fun FavoriteMoviesEmptyStatePreview() {
+    MovieAppTheme {
+        Surface {
+            FavoriteMoviesEmptyState()
+        }
+    }
+}
--- app/src/main/java/com/beti1205/movieapp/ui/account/widget/favoritemovies/FavoriteMoviesSection.kt
+++ app/src/main/java/com/beti1205/movieapp/ui/account/widget/favoritemovies/FavoriteMoviesSection.kt
@@ -4,14 +4,17 @@ import android.content.res.Configuration
 import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.tooling.preview.PreviewParameter
 import androidx.compose.ui.tooling.preview.PreviewParameterProvider
 import androidx.compose.ui.unit.dp
+import com.beti1205.movieapp.R
 import com.beti1205.movieapp.feature.movies.data.Movie
 import com.beti1205.movieapp.ui.theme.MovieAppTheme
 
@@ -22,11 +25,14 @@ fun FavoriteMoviesSection(
     modifier: Modifier = Modifier
 ) {
     DefaultCard {
-        Column(modifier = modifier) {
+        Column(modifier = modifier.fillMaxWidth()) {
             AccountSectionHeader(
-                text = "Favorite movies",
+                text = stringResource(R.string.favorite_movies_section_header),
                 modifier = Modifier.padding(top = 8.dp)
             )
+            AnimatedVisibility(visible = movies.isEmpty()) {
+                FavoriteMoviesEmptyState()
+            }
             AnimatedVisibility(
                 visible = movies.isNotEmpty()
             ) {

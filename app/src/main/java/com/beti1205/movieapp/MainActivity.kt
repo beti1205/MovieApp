f52602305aafffdcc36eed4189a8846dfa77f856
beti1205 <beta.bujalska@gmail.com>
2023-02-14 22:53:27 +0100

    refactor: Bottom navigation

--- app/src/main/java/com/beti1205/movieapp/MainActivity.kt
+++ app/src/main/java/com/beti1205/movieapp/MainActivity.kt
@@ -5,57 +5,157 @@
 
 package com.beti1205.movieapp
 
+import android.annotation.SuppressLint
 import android.os.Bundle
-import android.util.Log
-import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.isVisible
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.BottomNavigation
+import androidx.compose.material.BottomNavigationItem
+import androidx.compose.material.Icon
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Scaffold
+import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AccountBox
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.Tv
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
-import androidx.navigation.fragment.NavHostFragment
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.navigateUp
-import androidx.navigation.ui.setupWithNavController
-import com.beti1205.movieapp.databinding.ActivityMainBinding
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.currentBackStackEntryAsState
+import androidx.navigation.compose.rememberNavController
+import com.beti1205.movieapp.ui.theme.MovieAppTheme
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
-class MainActivity : AppCompatActivity() {
+class MainActivity : ComponentActivity() {
 
-    private lateinit var binding: ActivityMainBinding
-    private lateinit var appBarConfiguration: AppBarConfiguration
-    private lateinit var navController: NavController
-
+    @SuppressLint("UnusedMaterialScaffoldPaddingParameter")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
-
-        val navHostFragment = supportFragmentManager.findFragmentById(
-            R.id.myNavHostFragment
-        ) as NavHostFragment
-        navController = navHostFragment.navController
-
-        appBarConfiguration = AppBarConfiguration(
-            setOf(
-                R.id.movieFragment,
-                R.id.TVSeriesFragment,
-                R.id.accountFragment
-            )
-        )
-
-        binding.bottomNavigation.setupWithNavController(navController)
-
-        navController.addOnDestinationChangedListener { _, destination, arguments ->
-            Log.d("Navigation", "destination: $destination, arguments: $arguments")
-            binding.bottomNavigation.isVisible = destination.id in listOf(
-                R.id.movieFragment,
-                R.id.TVSeriesFragment,
-                R.id.accountFragment
-            )
+        setContent {
+            MovieAppTheme {
+                val navController = rememberNavController()
+                Scaffold(bottomBar = {
+                    BottomNavigationBar(navController)
+                }) {
+                    NavHost(navController = navController, startDestination = "home") {
+                        composable("home") {
+                            HomeScreen()
+                        }
+                        composable("tv series") {
+                            TVScreen()
+                        }
+                        composable("account") {
+                            AccountScreen()
+                        }
+                    }
+                }
+            }
         }
     }
+}
 
-    override fun onSupportNavigateUp(): Boolean {
-        return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
+@Composable
+private fun BottomNavigationBar(navController: NavHostController) {
+    BottomNavigationBar(
+        items = listOf(
+            BottomNavItem(
+                name = "Home",
+                route = "home",
+                icon = Icons.Default.Home
+            ),
+            BottomNavItem(
+                name = "TV Series",
+                route = "tv series",
+                icon = Icons.Default.Tv
+            ),
+            BottomNavItem(
+                name = "Account",
+                route = "account",
+                icon = Icons.Default.AccountBox
+            )
+
+        ),
+        navController = navController,
+        onItemCLick = { navController.navigate(it.route) }
+    )
+}
+
+@Composable
+fun HomeScreen() {
+    Box(
+        modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ) {
+        Text(text = "Home Screen")
+    }
+}
+
+@Composable
+fun TVScreen() {
+    Box(
+        modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ) {
+        Text(text = "TV Screen")
+    }
+}
+
+@Composable
+fun AccountScreen() {
+    Box(
+        modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ) {
+        Text(text = "Account Screen")
+    }
+}
+
+data class BottomNavItem(
+    val name: String,
+    val route: String,
+    val icon: ImageVector
+)
+
+@Composable
+fun BottomNavigationBar(
+    items: List<BottomNavItem>,
+    navController: NavController,
+    modifier: Modifier = Modifier,
+    onItemCLick: (BottomNavItem) -> Unit
+) {
+    val backStackEntry = navController.currentBackStackEntryAsState()
+    BottomNavigation(
+        modifier = modifier,
+        backgroundColor = MaterialTheme.colors.secondary,
+        elevation = 5.dp
+    ) {
+        items.forEach { item ->
+            val selected = item.route == backStackEntry.value?.destination?.route
+            BottomNavigationItem(
+                selected = selected,
+                onClick = { onItemCLick(item) },
+                selectedContentColor = Color.White,
+                unselectedContentColor = Color.Gray,
+                icon = {
+                    Column(horizontalAlignment = Alignment.CenterHorizontally) {
+                        Icon(imageVector = item.icon, contentDescription = null)
+                        Text(text = item.name)
+                    }
+                }
+            )
+        }
     }
 }

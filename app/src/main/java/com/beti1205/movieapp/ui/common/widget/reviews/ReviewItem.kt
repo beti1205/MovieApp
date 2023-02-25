1a8434cf46988316cab8e26b6849bb84745e5f2a
beti1205 <beta.bujalska@gmail.com>
2023-02-25 16:06:10 +0100

    improvement: Reviews - fix tests and refactor ReviewItem

--- app/src/main/java/com/beti1205/movieapp/ui/common/widget/reviews/ReviewItem.kt
+++ app/src/main/java/com/beti1205/movieapp/ui/common/widget/reviews/ReviewItem.kt
@@ -14,9 +14,12 @@ import androidx.compose.material.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.tooling.preview.PreviewParameter
 import androidx.compose.ui.unit.dp
+import coil.compose.AsyncImage
+import coil.request.ImageRequest
 import com.beti1205.movieapp.feature.reviews.data.Review
 import com.beti1205.movieapp.ui.common.widget.AccountAvatar
 import com.beti1205.movieapp.ui.common.widget.Avatar
@@ -33,45 +36,64 @@ fun ReviewItem(
         val avatar = item.authorDetails.avatar
 
         Row {
-            if (avatar != null) {
-                AccountAvatar(
-                    avatar = avatar,
-                    modifier = Modifier
-                        .weight(1f)
-                )
-                ReviewContent(
-                    text = item.content,
-                    modifier = Modifier
-                        .weight(3f)
-                        .padding(start = 8.dp)
-                )
-            } else {
-                Column(
-                    modifier = Modifier.weight(1f),
-                    horizontalAlignment = Alignment.CenterHorizontally
-                ) {
-                    Avatar(
-                        text = item.author,
-                        size = 70.dp
-                    )
-                }
-                ReviewContent(
-                    text = item.content,
-                    modifier = Modifier
-                        .weight(3f)
-                        .padding(start = 8.dp)
-                )
+            when {
+                avatar != null ->
+                    Column(modifier = Modifier.weight(1f)) {
+                        ReviewerAvatar(avatar = avatar)
+                    }
+                else -> DefaultReviewerAvatar(item = item, modifier = modifier.weight(1f))
             }
+            ReviewContent(
+                text = item.content,
+                modifier = Modifier
+                    .weight(3f)
+                    .padding(start = 8.dp)
+            )
         }
-        Row(modifier = Modifier.padding(vertical = 8.dp)) {
-            ReviewCreatedDate(item.createdDate, modifier = Modifier.weight(1f))
-            ReviewAuthor(item.author)
-        }
+        ReviewsInfo(item = item)
         if (index != reviews.size - 1) {
             Divider(
                 modifier = Modifier.padding(vertical = 8.dp)
             )
         }
+    }
+}
+
+@Composable
+private fun ReviewerAvatar(avatar: String) {
+    if (avatar.startsWith("/https")) {
+        AsyncImage(
+            model = ImageRequest.Builder(LocalContext.current)
+                .data(avatar.removePrefix("/"))
+                .crossfade(true)
+                .build(),
+            contentDescription = null
+        )
+    } else {
+        AccountAvatar(
+            avatar = avatar
+        )
+    }
+}
+
+@Composable
+private fun ReviewsInfo(item: Review) {
+    Row(modifier = Modifier.padding(vertical = 8.dp)) {
+        ReviewCreatedDate(item.createdDate, modifier = Modifier.weight(1f))
+        ReviewAuthor(item.author)
+    }
+}
+
+@Composable
+private fun DefaultReviewerAvatar(item: Review, modifier: Modifier = Modifier) {
+    Column(
+        modifier = modifier,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Avatar(
+            text = item.author,
+            size = 70.dp
+        )
     }
 }
 
